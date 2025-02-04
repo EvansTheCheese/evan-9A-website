0a5dd669b5f91874faaec48c090f9ff813423e99
EvansTheCheese <evandio.joel46@smp.belajar.id>
2025-02-04 11:42:26 +0700

    First upload

---
+++ Part 07 - 17 September/catatan part .py
@@ -0,0 +1,20 @@
+#fungsi input
+
+'''Membuat fungsi'''
+def hello_world():
+    '''fungsi menampilkan hello world'''
+    print("hello world")
+    print("Selamat")
+
+
+hello_world()
+hello_world()
+hello_world()
+
+''' fungsi dengan argument input '''
+
+'''rumus
+def nama_fungsi(argument):
+Badan fungsi'''
+
+def hello_world(nama:)
